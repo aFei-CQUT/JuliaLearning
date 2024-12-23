571a89fdedbb7477fcf78ad4f5a6283e20a5bfcf
aFeiCQUT <2039787966@qq.com>
2024-12-23 22:53:34 +0800

    update files

--- Chen/个人住房贷款模型.jl
+++ Chen/个人住房贷款模型.jl
@@ -1,8 +1,18 @@
 using SymPy
 
-# 使用 symbols 定义符号变量
+# 定义符号变量
 A, t, n, r, y = symbols("A t n r y")
 
-# 解析解公式
+# 等额本金还款模型
+# 计算第t期应还款金额
+y_t_expr = (A / n) + (A - (t - 1) / n * A) * r
+
+# 等额本息还款模型
+# 计算第t期尚欠银行的贷款额
 A_t_expr = (y / r) * (1 - ((1 + r)^t) / ((1 + r)^n))
-display(A_t_expr)
+
+# 打印解析解
+println("等额本金还款模型第t期应还款金额: ")
+println(y_t_expr)
+println("\n等额本息还款模型第t期尚欠银行的贷款额: ")
+println(A_t_expr)
